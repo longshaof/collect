1a27291df94b79d4ee00bcb414dad9877ff1bdb6
longshaof <fckfirewall@gmail.com>
2015-06-08 20:34:47 +0800

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -7,5 +7,40 @@ cd /opt
 git clone git://git.infradead.org/ocserv.git 
 cd ocser*
 sed -i s/#define MAX_CONFIG_ENTRIES 96//#define MAX_CONFIG_ENTRIES 250/g 
+/configure --prefix=/usr --sysconfdir=/etc --enable-linux-namespaces
+make&&make install
+mkdir /etc/ocserv
+cp doc/sample.config /etc/ocserv/ocserv.conf
+mkdir certificates&&cd certificates 
+
+cat << _EOF_ >ca.tmpl
+cn = "Boybeta.com VPN"
+organization = "Boybeta.com"
+serial = 1
+expiration_days = 3650
+ca
+signing_key
+cert_signing_key
+crl_signing_key
+_EOF_
+certtool --generate-privkey --outfile ca-key.pem
+certtool --generate-self-signed --load-privkey ca-key.pem --template ca.tmpl --outfile ca-cert.pem
+
+cat << _EOF_ >server.tmpl
+cn = "Your hostname or IP"
+organization = "Your fancy name"
+expiration_days = 3650
+signing_key
+encryption_key
+tls_www_server
+_EOF_
+
+certtool --generate-privkey --outfile server-key.pem
+certtool --generate-certificate --load-privkey server-key.pem --load-ca-certificate ca-cert.pem --load-ca-privkey ca-key.pem --template server.tmpl --outfile server-cert.pem
+
+sudo cp ca-cert.pem /etc/ssl/private/my-ca-cert.pem
+sudo cp server-cert.pem /etc/ssl/private/my-server-cert.pem
+sudo cp server-key.pem /etc/ssl/private/my-server-key.pem
+
 
 pwd
